71837aeb911228b15422b374c7633b152adcc116
Nebojsa Dimic <nsd4@rice.edu>
2024-01-01 11:49:09 +0100

    Finalized CRUD operations for FastAPI

--- app/main.py
+++ app/main.py
@@ -118,22 +118,92 @@ def get_cat(id:str, db: Session=Depends(get_db)):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
     return {"cat": cat}
 
+@app.delete("/cat/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_cat(id: str, db: Session = Depends(get_db)) -> None:
+    cat = db.query(models.Cat).filter(models.Cat.id == id)      
     
+    if cat.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Cat with id: {id} does not exist")
+    cat.delete(synchronize_session=False)
+    db.commit()    
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)    
+
+@app.delete("/category/delete/{name}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_category(name: str, db: Session = Depends(get_db)):
+    category = db.query(models.Category).filter(models.Category.name == name)      
+    
+    if category.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Category with name: {name} does not exist")
+    category.delete(synchronize_session=False)
+    db.commit()    
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)  
+ 
+@app.delete("/breed/delete/{name}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_breed(name: str, db: Session = Depends(get_db)):
+    breed = db.query(models.Breed).filter(models.Breed.id == name)      
+    
+    if breed.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Breed with name: {name} does not exist")
+    breed.delete(synchronize_session=False)
+    db.commit()    
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)    
+
+@app.delete("/cat_category/delete/{cat_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_cat_category(cat_id: str, db: Session = Depends(get_db)):
+    cat_category = db.query(models.Cat_Category).filter(models.Cat_Category.cat_id == cat_id)      
+    
+    if cat_category.first() == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Cat_Category with name: {cat_id} does not exist")
+    cat_category.delete(synchronize_session=False)
+    db.commit()    
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)   
+
+
+@app.put("/cat/update/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_cat(id: str, updated_cat: Cat, db: Session = Depends(get_db)):
+    update_query = db.query(models.Cat).filter(models.Cat.id == id)
+    post = update_query.first()
+    
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Cat with id: {id} does not exist")
+        
+    update_query.update(updated_cat.dict(), synchronize_session=False)    
+    
+    db.commit()
+    
+    return {"message": "success"} 
+
+@app.put("/category/update/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_category(id: int, updated_category: Category, db: Session = Depends(get_db)):
+    update_query = db.query(models.Category).filter(models.Category.id == id)
+    post = update_query.first()
+    
+    if post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Category with id: {id} does not exist")
+    
+    aaa = updated_category.dict()
+    print(aaa)
+        
+    update_query.update(updated_category.dict(), synchronize_session=False)    
+    
+    db.commit()
+    
+    return {"message": "success"} 
 
 #####
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)) -> None:
-    post = db.query(models.Post).filter(models.Post.id == id)      
-    
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id: {id} does not exist")
-    post.delete(synchronize_session=False)
-    db.commit()    
-    
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.put("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)) ->None:
