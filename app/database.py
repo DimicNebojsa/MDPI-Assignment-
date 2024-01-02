cbeae44eff3b40c6e5642a6eb83a8061cea0bc5f
Nebojsa Dimic <nsd4@rice.edu>
2024-01-02 17:30:12 +0100

    database.py added comments

--- app/database.py
+++ app/database.py
@@ -1,12 +1,12 @@
+"""Package for databse engine and session.
+
+Author: Nebojsa Dimic
+Date: 1/2/2024
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
-
-#engine = create_engine(SQLALCHEMY_DATABASE_URL)   
-
 
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/mdpi"
@@ -19,6 +19,17 @@ Base = declarative_base()
 
 # Dependency
 def get_db() -> SessionLocal:
+    """Generator that provides local session."""
+    """
+        Args:
+            None
+                
+        Excpetions:
+            HTTP exceptions    
+
+        Returns:
+            SessionLocal
+    """
     db = SessionLocal()
     try:
         yield db
